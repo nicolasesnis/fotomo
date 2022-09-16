c81ba8f4032f33b64305c280c6c1fa6d08c14f67
Nicolas Esnis <nicolas.esnis@gmail.com>
2022-09-16 10:49:08 -0700

    user accounts management

--- pages/👤 Mon Compte.py
+++ pages/👤 Mon Compte.py
@@ -37,52 +37,56 @@ def end_session():
     cookie_manager.delete('user_cookie')
 
 
+
+    
 if 'user_cookie' not in cookie_manager.get_all():
     
     all_users = pd.read_csv('tmp')
     tab1, tab2 = st.tabs(["Client existant", "Nouveau Client"])
     with tab1:
-        with st.form("login_form"):
-            st.write("Connectez-vous pour accéder à votre espace client")
-            email = st.text_input('Adresse Email')
-            password = st.text_input('Mot de passe')
-            submitted = st.form_submit_button("Se Connecter")
-            if submitted:
-                user = all_users[(all_users.email == email) & (all_users.password == password)]
-                if len(user) == 0:
-                    st.error('Aucun utilisateur ne correspond')
-                else:
-                    st.success('Vous êtes connecté !')
-                    start_session(user.loc[0].to_dict())
+        form = st.form("login_form")
+        form.write("Connectez-vous pour accéder à votre espace client")
+        email = form.text_input('Adresse Email')
+        password = form.text_input('Mot de passe')
+        submitted = form.form_submit_button("Se Connecter")
+        if submitted:
+            user = all_users[(all_users.email == email) & (all_users.password == password)]
+            if len(user) == 0:
+                st.error('Aucun utilisateur ne correspond')
+            else:
+                st.success('Vous êtes connecté !')
+                start_session(user.iloc[0].to_dict())
+                
                     
     with tab2:
-        with st.form("register_form"):
-            name = st.text_input('Prénom')
-            surname = st.text_input('Nom de famille')
-            email = st.text_input('Adresse Email')
-            password = st.text_input('Mot de passe')
-            submitted = st.form_submit_button("S'inscrire")
-            if submitted:
-                if email in all_users.email.unique():
-                    st.error('Cette adresse email est déjà associée à un compte Fotomo.')
-                elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
-                    st.error('Veuillez entrer une adresse email valide.')
-                elif email == '' or name == '' or password == '' or surname == '':
-                    st.error('Veuillez renseigner tous les champs.')
-                elif len(password)  < 6:
-                    st.error('Mot de passe trop court.')
-                else:
-                    all_users.loc[len(all_users.index)] = [name,	surname,	email,	password] 
-                    all_users.to_csv('tmp', index=None)
-                    upload_s3_file('tmp', 's3://fotomo-secrets/website_users.csv')
-                    user = {
-                            'name': name,
-                            'surname': surname,	
-                            'email': email,	
-                            'password': password
-                        }
-                    st.success('Vous êtes connecté !')
-                    start_session(user)
+        form = st.form("register_form")
+        name = form.text_input('Prénom')
+        surname = form.text_input('Nom de famille')
+        email = form.text_input('Adresse Email')
+        password = form.text_input('Mot de passe')
+        submitted = form.form_submit_button("S'inscrire")
+        if submitted:
+            if email in all_users.email.unique():
+                st.error('Cette adresse email est déjà associée à un compte Fotomo.')
+            elif not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+                st.error('Veuillez entrer une adresse email valide.')
+            elif email == '' or name == '' or password == '' or surname == '':
+                st.error('Veuillez renseigner tous les champs.')
+            elif len(password)  < 6:
+                st.error('Mot de passe trop court.')
+            else:
+                all_users.loc[len(all_users.index)] = [name,	surname,	email,	password] 
+                all_users.to_csv('tmp', index=None)
+                upload_s3_file('tmp', 's3://fotomo-secrets/website_users.csv')
+                user = {
+                        'name': name,
+                        'surname': surname,	
+                        'email': email,	
+                        'password': password
+                    }
+                st.success('Vous êtes connecté !')
+                start_session(user)
+                
 else:
     st.button("Déconnexion", on_click=end_session)
     user_cookie = cookie_manager.get('user_cookie')
