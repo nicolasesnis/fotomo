6a19e17e21e78d88537396062eb83e4bb67a0a2e
Nicolas Esnis <nicolas.esnis@gmail.com>
2022-09-16 07:57:09 -0700

    allow image change

--- pages/📝 Créer mon mot.py
+++ pages/📝 Créer mon mot.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from src.s3.list_photos import list_bucket
+from random import randrange
+
 
 st.set_page_config(
 	# layout = "centered",
@@ -16,23 +18,81 @@ def load_letters():
     letters = {}
     for photo in all_albums:
         album = photo['Key'].split('/')[0]
-        if album not in ['Logos', 'Galerie']:
+        if album not in ['Logos', 'Galerie'] and photo['Key'].split('/')[1] != "":
             if album not in letters.keys():
-                letters[album] = []
-            letters[album].append('https://fotomo.s3.amazonaws.com/' + photo['Key'])
+                letters[album] = {}
+            letters[album][photo['Key'].split('/')[1].split('.')[0]] = {'path': 'https://fotomo.s3.amazonaws.com/' + photo['Key']}
     return letters 
-letters = load_letters()
+letters_photos = load_letters()
 
-text = st.text_input('Entrez un mot ou une phrase...')
+
+def get_next_index(letter, text_dict):
+    global letters_photos
+    used_indices = [text_dict[key]['index'] for key in text_dict.keys() if text_dict[key]['letter'] == letter]
+    if max(used_indices) + 1 < len(letters_photos[letter].keys()):
+        return max(used_indices) + 1
+    else:
+        return randrange(len(letters_photos[letter].keys()))
+    
+
+def update_photo(letter, index):
+    global letters_photos
+    photo_name = list(letters_photos[letter])[index]
+    return {
+        'index': index,
+        'letter_photo_name': photo_name,
+        'letter_photo_path': letters_photos[letter][photo_name]['path']
+    }
+
+def func(text):
+    st.write(text)
+    
+def set_allow_reset_text_dict():
+    st.session_state['allow_reset_text_dict'] = True
+    
+text = st.text_input('Entrez un mot ou une phrase...', on_change=set_allow_reset_text_dict)
+
+if 'allow_reset_text_dict' in st.session_state and  st.session_state['allow_reset_text_dict']:
+    text_dict = {}
+    for letter_index, letter in enumerate(list(text)):
+        letter = letter.upper()
+        if letter not in letters_photos.keys():
+            continue
+        if letter not in [text_dict[key]['letter'] for key in text_dict.keys()]:
+            index  = 0
+        else:
+            index = get_next_index(letter, text_dict)
+        text_dict[letter_index] = {'letter': letter}
+        photo_name = list(letters_photos[letter])[index]
+        text_dict[letter_index].update(
+            update_photo(letter, index)
+        )    
+        if  text_dict != {}:
+            st.session_state['text_dict'] = text_dict
+
+
 if text != '':
     cols = st.columns(len(list(text)))
     for letter_index, col in enumerate(cols):
         letter = list(text)[letter_index].upper()
         with col:  
-            if letter in letters.keys():
-                photo_index = st.number_input('', min_value=0, max_value=len(letters[letter]) -1, key=str(letter_index))
-                image = letters[letter][photo_index]
+            if letter in letters_photos.keys():
+                if st.button('Changer',  key = str(letter_index)):
+                    st.session_state['allow_reset_text_dict'] = False
+                    index = get_next_index(letter, st.session_state['text_dict'])
+                    st.session_state['text_dict'][letter_index].update(update_photo(letter, index))
+                image = st.session_state['text_dict'][letter_index]['letter_photo_path']
                 st.image(image, use_column_width='auto')
+                # header_html = '<img src="' + image + '" style="width: 50%; height: 50%"/>' 
+                # st.markdown(
+                #     header_html, unsafe_allow_html=True,
+                # )
+
                 
             else:
                 st.write(letter)
+
+if 'text_dict' in st.session_state:
+    st.write('Photos sélectionnées:')
+    st.write(st.session_state['text_dict'])
+
