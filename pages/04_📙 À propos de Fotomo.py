cff58a0eeaa3c80bfb4bdd66d0f6f284dd064d80
Nicolas Esnis <nicolas.esnis@gmail.com>
2022-09-16 11:01:20 -0700

    about fotomo text

---
+++ pages/04_📙 À propos de Fotomo.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title('@Maman envoie moi du contenu')
+st.write('Le concept / mon histoire')
+st.write('Légal: siège social, contacts, etc')
+st.write('Conditions Générales de Vente')
+st.write('Modalités de livraison')
+st.write('Retours')
+st.write('Politique de confidentialité / données personnelles')
